29998b3ae2cdbc8dbb093712206a6e854d64178b
Nii-101 <noelamptey101@gmail.com>
2024-09-23 13:07:59 +0000

    build: :package: setup routing, file structure and installed axios and framer motion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
+import {Route, Routes} from 'react-router-dom';
+import Home from './pages/homepage';
+
 function App() {
   return (
-    <>
-      <div className="flex min-h-screen w-full items-center justify-center font-bold text-red-500 text-3xl animate-bounce underline">
-        Hello World!
-      </div>
-    </>
+    <Routes>
+      <Route index element={<Home />} />
+        <Route path='/home' element={<Home />} />
+    </Routes>
   );
 }
 
